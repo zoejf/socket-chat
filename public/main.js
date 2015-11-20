212ba7e9e968ab1f139731ee252cb88fd83d2b59
zoejf <zoejfoss@gmail.com>
2015-11-19 16:18:21 -0800

    saves username from prompt

--- public/main.js
+++ public/main.js
@@ -8,13 +8,19 @@ $(function() {
 	var source = $('#messages-template').html();
 	var template = Handlebars.compile(source);
 
+	function welcome(){
+		var answer = prompt("What is your username?");
+		return answer;
+	}
+	var username = welcome();
+
 	// submit form to send a message
 	$('#send-msg').on('submit', function (event) {
 		event.preventDefault();
 
 		//get new message from form input
 		var newMsg = $('#new-msg').val();
-		var username = $('#username').val();
+		// var username = $('#username').val();
 
 		//send new message to socket (server)
 		socket.emit('chat message', newMsg, username);
